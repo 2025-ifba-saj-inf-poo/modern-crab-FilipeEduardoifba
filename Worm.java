c5fab78a5cf49635782d5cae3294a96e979d3ed3
KLEBERSONKJS <20241TI0014@ifba.edu.br>
2025-08-25 11:46:39 -0300

    fim decapota

---
+++ Worm.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Worm here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Worm extends Actor
+{
+    
+}
